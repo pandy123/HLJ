bfcc0fd3bf53cd14b45ed73a464e47827b9f4c3d
HUANG.YB <huangyanbei123@126.com>
2020-10-21 15:29:32 +0800

    'compile'

---
+++ examle/build/HLJ.js
@@ -0,0 +1,15 @@
+var HLJ = (function (exports) {
+    'use strict';
+
+    function main() {
+        console.log("dd");
+    }
+
+    exports.main = main;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+    return exports;
+
+}({}));
+this.HLJ = HLJ;
---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+
+import transpile from 'rollup-plugin-buble';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import { string } from 'rollup-plugin-string';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+
+export default{
+    input:'./src/index.ts',
+    output:{
+        file:'./examle/build/HLJ.js',
+        format:'iife',
+        name:'HLJ',
+        footer:'this.HLJ = HLJ;'
+    },
+  plugins : [
+        sourcemaps(),
+        nodeResolve({
+        }),
+        commonjs({
+        }),
+        typescript(),
+        string({
+            include: [
+                '**/*.frag',
+                '**/*.vert',
+            ],
+        }),
+        replace({
+            __VERSION__: '1.0.1',
+        }),
+        transpile(),
+    ],
+}
